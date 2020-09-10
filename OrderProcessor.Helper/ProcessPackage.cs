088a0e24e2a9990dc69eb53aae7b22f743774e53
Mahesh <mahesh.pusa@allscripts.com>
2020-09-10 18:50:14 +0530

    version 3

--- OrderProcessor.Helper/ProcessPackage.cs
+++ OrderProcessor.Helper/ProcessPackage.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿using OrderProcessor.BusinessRules.Abstract;
+using System;
 
 namespace OrderProcessor.Helper
 {
     public class ProcessPackage
     {
+        IBusinessRule _businessRule;
+        public ProcessPackage(IBusinessRule businessRule)
+        {
+            _businessRule = businessRule;
+        }
+        public void processpackage(string prodcutCategory)
+        {
+            prodcutCategory
+        }
     }
 }
